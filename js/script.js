89355dc42d46bec61c1f2c72bdb48eefd2ec4da1
olkobarylko <olegbarylko81@gmail.com>
2023-02-12 19:10:37 +0200

    asdas

--- js/script.js
+++ js/script.js
@@ -1,86 +1,3 @@
-// let numbers = document.querySelectorAll('.number');
-
-// function offset(el) {
-//     const rect = el.getBoundingClientRect(),
-//         scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-//         scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-//     return {
-//         top: rect.top + scrollTop,
-//         left: rect.left + scrollLeft
-//     }
-// }
-
-// function olko() {
-//     if (window.pageYOffset == offset(numbers[0]).top) {
-//         numbers.forEach((item) => {
-//             let count = +item.innerHTML;
-//             let sum = 0;
-//             let a = (2000 / +item.innerHTML);
-//             let inteval = setInterval(function () {
-//                 sum++;
-//                 if (count > 1000) {
-//                     sum += 9;
-//                 }
-//                 if (sum == count) {
-//                     clearInterval(inteval);
-//                 }
-//                 item.innerHTML = sum;
-//             }, a)
-//         })
-//     }
-//     console.log('top', offset(numbers[0]).top)
-//     console.log('scroll', window.pageYOffset)
-// }
-
-// window.addEventListener('scroll', olko)
-
-// var number = document.querySelector('.number'),
-//     numberTop = number.getBoundingClientRect().top,
-//     start = +number.innerHTML,
-//     end = +number.dataset.max;
-
-// window.addEventListener('scroll', function onScroll() {
-//     if (window.pageYOffset > numberTop - window.innerHeight / 2) {
-//         this.removeEventListener('scroll', onScroll);
-//         var interval = setInterval(function () {
-//             number.innerHTML = ++start;
-//             if (start == end) {
-//                 clearInterval(interval);
-//             }
-//         }, 1)
-//     }
-// })
-
-let numbers = document.querySelectorAll('.number');
-var bodyRect = document.body.getBoundingClientRect();
-var rect = numbers[0].getBoundingClientRect();
-numberTop = {
-    top: rect.top - bodyRect.top
-};
-
-numbers.forEach(item => {
-    var start = +item.innerHTML,
-        end = +item.dataset.max;
-
-    window.addEventListener('scroll', function onScroll() {
-        if (window.pageYOffset > (numberTop.top - window.innerHeight)) {
-            this.removeEventListener('scroll', onScroll);
-            let a = (2300 / end);
-            var interval = setInterval(function () {
-                if (+end > 1000) {
-                    start += 9
-                    item.innerHTML = ++start;
-                } else {
-                    item.innerHTML = ++start;
-                }
-                if (start == end) {
-                    clearInterval(interval);
-                }
-            }, a)
-        }
-    })
-});
-
 function testWebP(callback) {
 
   var webP = new Image();
@@ -98,57 +15,65 @@ testWebP(function (support) {
   }
 });
 
-var insurerSwiper = new Swiper(".insurers-section__swiper", {
-  slidesPerView: 1,
-  autoplay: {
-    delay: 1000,
-  },
-  breakpoints: {
-    550: {
-      slidesPerView: 2,
-    },
-    850: {
-      slidesPerView: 3,
-    },
-    1000: {
-      slidesPerView: 4,
-    },
-    1250: {
-      slidesPerView: 5,
-    },
-    1600: {
-      slidesPerView: 6,
-    },
-  },
-  spaceBetween: 30,
-  grabCursor: true,
-  pagination: {
-    el: ".insurers-section__swiper-pagination",
-    clickable: true,
-  },
-});
+let insurersSwiper = document.querySelector('.insurers-section__swiper');
 
-var swiper = new Swiper(".pillars-section__swiper", {
-  spaceBetween: 25,
-  autoplay: {
-    delay: 2000,
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
+if (insurersSwiper) {
+  var insurerSwiper = new Swiper(".insurers-section__swiper", {
+    slidesPerView: 1,
+    autoplay: {
+      delay: 1000,
     },
-    850: {
-      slidesPerView: 2,
+    breakpoints: {
+      550: {
+        slidesPerView: 2,
+      },
+      850: {
+        slidesPerView: 3,
+      },
+      1000: {
+        slidesPerView: 4,
+      },
+      1250: {
+        slidesPerView: 5,
+      },
+      1600: {
+        slidesPerView: 6,
+      },
     },
-    1250: {
-      slidesPerView: 3,
+    spaceBetween: 30,
+    grabCursor: true,
+    pagination: {
+      el: ".insurers-section__swiper-pagination",
+      clickable: true,
     },
-  },
-  pagination: {
-    el: ".pillars-section__swiper-pagination",
-    clickable: true,
-  },
-});
+  });
+}
+
+let pillarsSwiper = document.querySelector('.pillars-section__swiper');
+
+if (pillarsSwiper) {
+  var swiper = new Swiper(".pillars-section__swiper", {
+    spaceBetween: 25,
+    autoplay: {
+      delay: 2000,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      850: {
+        slidesPerView: 2,
+      },
+      1250: {
+        slidesPerView: 3,
+      },
+    },
+    pagination: {
+      el: ".pillars-section__swiper-pagination",
+      clickable: true,
+    },
+  });
+}
 
 let burger_btn = document.querySelector('.header__burger-btn');
 let burger_menu = document.querySelector('.header__burger-menu');
@@ -197,8 +122,10 @@ let advantageListBottomStatus = true;
 
 let mql1 = window.matchMedia('(max-width: 768px)');
 
-if (mql1.matches) {
-  advantageListTop.style.paddingBottom = advantageListTop.scrollHeight + 81 + 'px';
+if (advantageListTop) {
+  if (mql1.matches) {
+    advantageListTop.style.paddingBottom = advantageListTop.scrollHeight + 81 + 'px';
+  }
 }
 
 function advantageListTopShow() {
@@ -233,3 +160,48 @@ function advantageListBottomShow() {
 advantagesTopMoreItem.addEventListener('click', advantageListTopShow);
 
 advantagesBottomMoreItem.addEventListener('click', advantageListBottomShow);
+
+let numbers = document.querySelectorAll('.number');
+if (numbers) {
+  var bodyRect = document.body.getBoundingClientRect();
+  var rect = numbers[0].getBoundingClientRect();
+  numberTop = {
+    top: rect.top - bodyRect.top
+  };
+
+  numbers.forEach(item => {
+    var start = +item.innerHTML,
+      end = +item.dataset.max;
+
+    window.addEventListener('scroll', function onScroll() {
+      if (window.pageYOffset > (numberTop.top - window.innerHeight)) {
+        this.removeEventListener('scroll', onScroll);
+        let a = (2300 / end);
+        var interval = setInterval(function () {
+          if (+end > 1000) {
+            start += 9
+            item.innerHTML = ++start;
+          } else {
+            item.innerHTML = ++start;
+          }
+          if (start == end) {
+            clearInterval(interval);
+          }
+        }, a)
+      }
+    })
+  })
+}
+
+function redirectBra() {
+  if (location.href.includes('bra/')) {} 
+  else { 
+    location.href = location.href.slice(0, (location.href.lastIndexOf('/') + 1)) + "bra/" + location.href.slice((location.href.lastIndexOf('/') + 1));
+  }
+}
+
+function redirectArg() {
+  if (location.href.includes('bra/')) {
+    location.href = location.href.replace('bra/', '')
+  }
+}
